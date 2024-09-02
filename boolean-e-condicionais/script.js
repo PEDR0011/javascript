15a0b2e4b3c7cea5f5fc545becc4fb53d71eea1c
Pedro Henrique <164585741+PEDR0011@users.noreply.github.com>
2024-09-02 12:33:23 -0300

    praticando

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -8,7 +8,43 @@
   - "" ou '' ou ``
 
  Todo o resto é true 
-  */
+
+let  a = 6;
+let b = "10";
+let c = 10 + 9;
+let resultado = c + a - b;
+console.log(resultado);
+
+let tuaMae = 300 + 4;
+let haha = "202";
+haha = +haha;
+console.log(tuaMae + haha++);
+console.log(haha);
+
+let igual = "Igualzinho mano";
+let diferente = "Nada ave"
+let numero = 0.10;
+if(numero === 10) {
+  console.log(igual)
+} else {
+  console.log(diferente);
+}
+
+
+let conta = +"10" + 90 * 4 / 2;
+console.log(conta--);
+console.log(conta);
+
+let x = 200;
+let y = +"200"; 
+
+if(x !== y) {
+  console.log("Diferentes");
+} else if(x === y) {
+  console.log("Iguais");
+} 
+
+*/
 
 var pedroEstuda = 0;
 if(pedroEstuda) {
@@ -30,4 +66,3 @@ console.log(!pedroTemDinheiro);
 
 let x = 10 !== "10";
 console.log(x);
-
