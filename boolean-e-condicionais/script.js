16c43c867f38620f414683a4d3665e624e6c27f1
Pedro Henrique <164585741+PEDR0011@users.noreply.github.com>
2024-09-02 14:35:58 -0300

    exercicio

--- boolean-e-condicionais/script.js
+++ boolean-e-condicionais/script.js
@@ -46,6 +46,7 @@ if(x !== y) {
 
 */
 
+/*
 var pedroEstuda = 0;
 if(pedroEstuda) {
   console.log("Sim");
@@ -66,3 +67,98 @@ console.log(!pedroTemDinheiro);
 
 let x = 10 !== "10";
 console.log(x);
+*/
+
+/*
+// Operador Lógico &&
+  Se ambos os valores forem true ele irá retornar o último valor verificado, Se algum valor for false ele irá retornar o mesmo e não irá continuar a verificar os próximos.
+*/
+if((50 > 70) && (40 + 10)) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+if(("haha" + 90) && (1 == 1)) {
+  console.log("verdadeiro");
+} else {
+  console.log("Falso");
+}
+/*
+// Operador Lógico ||
+  Retoornar o primeiro valor true que encontrar
+*/
+let ou = (1 - 1) || (10 + 1);
+if(ou) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+/*
+// Switch case
+  Com o switch você pode verificar se uma variável é igual à diferentes valores utilizando o case. Caso ela seja igual, você pode fazer alguma coisa e utilizar a palavra break; para cancelar a continuação. O valor de default ocorrerá caso nenhuma das anteriores seja verdadeira. 
+*/
+var ondeDormiu = "cama";
+
+switch(ondeDormiu) {
+  case "sofa":
+    console.log("Vai pra cama fdp");
+    break;
+  case "mesa":
+    console.log("Sai dai mano");
+    break;
+  case "banho":
+    console.log("Coitado mano");
+    break;
+    case "cama":
+      console.log("Ai sim");
+      break;
+    default:
+      console.log("Dormiu aonde?");
+}
+
+// EXERCICIO 
+
+  const minhaIdade = 19;
+  const outraPessoa = 27;
+  if(minhaIdade > outraPessoa) {
+    console.log("Pedro é mais velho");
+  } else if(minhaIdade === outraPessoa) {
+    console.log("Ambos tem a mesma idade");
+  } else {
+    console.log("A outra pessoa é mais velho");
+  }
+
+  var expressao = (5 - 2) && (5 - " ") && (5 - 2);
+  console.log(expressao);
+
+  var nome = "Pedro";
+  var idade = 19;
+  var possuiGraduacao = false;
+  var empregoFuturo;
+  var dinheiroNaConta = 0;
+  console.log(!!nome, !!idade, !!possuiGraduacao, !!empregoFuturo, !!dinheiroNaConta);
+
+  let brasil = 207;
+  let china = 1340;
+  if(brasil > china) {
+    console.log("Brasil tem mais Habitantes");
+  } else {
+    console.log("China tem mais Habitantes");
+  }
+
+  if(("Gato" === "gato") && (5 > 2)) {
+    console.log("Verdadeiro");
+  } else {
+    console.log("Falso");
+  }
+
+  if(("Gato" === "gato") || (5 > 2)) {
+    console.log("Gato" && "Cão");
+  } else {
+    console.log("falso");
+  }
+
+  
+  
