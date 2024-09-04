7a3b5d8088d92fbd4bd0b364dcb36d35b85ca51b
Pedro Henrique <164585741+PEDR0011@users.noreply.github.com>
2024-09-04 17:54:48 -0300

    exercicio

--- function/function.js
+++ function/function.js
@@ -1,3 +1,4 @@
+/*
 function calculaQuadrado(lado) {
   return lado * lado;
 }
@@ -50,3 +51,85 @@ function suaIdade(idade) {
   }
 }
 suaIdade();
+
+function taVeio(iidade) {
+  if(typeof iidade !== "number") {
+    return "Só pode numeros";
+  }else if(iidade >= 60) {
+    return true;
+  }else {
+    return false;
+  }
+}
+console.log(taVeio(60));
+
+function totalPaises(paisesVisitado) {
+  var totalPaises = 193
+  totalPaises = totalPaises - `${paisesVisitado}`;
+  return console.log(`Falta visitar ${totalPaises} pais.`);
+}
+totalPaises(10);
+
+function teste() {
+  var nome = "Pedro";
+  var sobreNome = "Henrique";
+function teste2() {
+  var ano = 18;
+  var sexo = "Masculino";
+  return `${nome}, ${sobreNome}, ${ano}, ${sexo}`
+}
+return teste2()
+
+}
+console.log(isNaN(teste()))
+var pirulito = "morango";
+console.log(pirulito);
+*/
+
+
+// EXERCICIO
+
+function trueOrFalse(valor) {
+  return !!valor
+}
+console.log(trueOrFalse(-1));
+
+function aoQuadrado(lado) {
+  return lado * 4;
+}
+console.log(aoQuadrado(5))
+
+function nameComplete(nome, sobreNome) {
+  return console.log(nome, sobreNome);
+}
+nameComplete("Pedro", "Henrique")
+
+function parOrImpar(numero) {
+  var modulo = numero % 2;
+  if(typeof modulo !== "number") {
+    return console.log("Apenas numeros!");
+  }else if(modulo === 0) {
+    return console.log("Par");
+  }else {
+    return console.log("Impar");
+  }
+  }
+parOrImpar(35364);
+
+function manda(dado) {
+  return console.log(typeof dado);
+}
+
+manda(6)
+addEventListener("click", function() {
+  console.log("Pedro Henrique");
+})
+
+var totalPaises = 193;
+function precisoVisitar(paisesVisitados) {
+  return `Ainda faltam ${totalPaises - paisesVisitados} países para visitar`;
+}
+function jaVisitei(paisesVisitados) {
+  return `Já visitei ${paisesVisitados} do total de ${totalPaises} países`;
+}
+console.log(precisoVisitar(20), jaVisitei(20))
