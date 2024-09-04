368e4da3ba04a64eff48fa60bd36777bafa7688d
Pedro Henrique <164585741+PEDR0011@users.noreply.github.com>
2024-09-04 10:59:32 -0300

    aprendendo function

---
+++ function/function.js
@@ -0,0 +1,29 @@
+function calculaQuadrado(lado) {
+  return lado * lado;
+}
+console.log(calculaQuadrado(10));
+
+let x = 2 * seiLa();
+function seiLa() {
+  return 10;
+}
+console.log(x)
+
+function imc(peso, altura) {
+  let imc = peso / (altura ** 2);
+  return imc; 
+}
+console.log(imc(55, 1.75));
+
+function corFavorita(cor) {
+  if(cor === "preto") {
+    return console.log("Uau você é das travas!!");
+} else if(cor === "verde") {
+  return console.log("Maconheiro");
+} else if(cor === "vermelho") {
+  return console.log("Sangue");
+} else {
+  return console.log("cor não encontrada");
+}
+}
+corFavorita("preto");
