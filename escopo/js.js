8cce1adbc44d1d5d1815ac7240d47b4f6d4fd3d9
Pedro Henrique <164585741+PEDR0011@users.noreply.github.com>
2024-09-15 09:01:39 -0300

    escopo

---
+++ escopo/js.js
@@ -0,0 +1,41 @@
+{
+  var nome = "Pedro"; // o var vaza a variavel para fora do bloco
+  let sobreNome = "Costa" // Já o let e const não vaza pra fora do bloco
+  console.log(nome);
+}
+console.log(nome);
+// console.log(sobreNome);
+
+
+//  EXERCICIO
+
+// Por qual motivo o código abaixo retorna com erros?
+
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas);
+}
+
+// Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero <= 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
